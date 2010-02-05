474f5495072cfd88dc739ee9ade1d31f2a1847b7
putkonen.matti <putkonen.matti@5f7b3a47-2d35-0410-89e4-8193f51d856c>
2010-02-05 03:46:31 +0000

    Fix for indention

--- trunk/vendors/cssmin.php
+++ trunk/vendors/cssmin.php
@@ -32,12 +32,12 @@ class cssmin
 	 */
 	public static function minify($v) 
     {
-    $v = trim($v);
-    $v = str_replace("\r\n", "\n", $v);
+	    $v = trim($v);
+	    $v = str_replace("\r\n", "\n", $v);
         $search = array("/\/\*[\d\D]*?\*\/|\t+/", "/\s+/", "/\}\s+/");
         $replace = array(null, " ", "}\n");
-    $v = preg_replace($search, $replace, $v);
-    $search = array("/\\;\s/", "/\s+\{\\s+/", "/\\:\s+\\#/", "/,\s+/i", "/\\:\s+\\\'/i", "/\\:\s+([0-9]+|[A-F]+)/i");
+	    $v = preg_replace($search, $replace, $v);
+	    $search = array("/\\;\s/", "/\s+\{\\s+/", "/\\:\s+\\#/", "/,\s+/i", "/\\:\s+\\\'/i", "/\\:\s+([0-9]+|[A-F]+)/i");
         $replace = array(";", "{", ":#", ",", ":\'", ":$1");
         $v = preg_replace($search, $replace, $v);
         $v = str_replace("\n", null, $v);
